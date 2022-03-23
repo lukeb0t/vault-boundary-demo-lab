6a6e5ad754712cb07397621556208cca13c4510a
Luke McCleary <luke.mccleary@gmail.com>
2022-03-23 14:34:51 -0400

    added plan and upgrade options to script

--- zt-lab.sh
+++ zt-lab.sh
@@ -1,15 +1,24 @@
 ACTION=$1
 WORKDIR=$(pwd)
 
+INFRA_PATH="${WORKDIR}/docker-infra"
+CONFIG_PATH="${WORKDIR}/config"
+
 
 if [ "${ACTION}" = "up" ]; then
-  terraform -chdir=${WORKDIR}/docker-infra init && terraform -chdir=-chdir=${WORKDIR}/config init
-  terraform -chdir=${WORKDIR}/docker-infra apply -auto-approve=true && terraform -chdir=/Users/lukemccleary/hashi-labs/vault-boundary-demo-lab/config apply -auto-approve=true
+  terraform -chdir=$INFRA_PATH apply -auto-approve=true && terraform -chdir=$CONFIG_PATH apply -auto-approve=true
 fi
 
 if [ "${ACTION}" = "down" ]; then
-  terraform -chdir=${WORKDIR}/docker-infra apply -destroy -auto-approve=true
-  rm ${WORKDIR}/docker-infra/terraform.tfstate*
-  rm ${WORKDIR}/config/terraform.tfstate*
+  terraform -chdir=$CONFIG_PATH apply -destroy -auto-approve=true
+  rm $CONFIG_PATH/terraform.tfstate*
+  rm $CONFIG_PATH/terraform.tfstate*
 fi
 
+if [ "${ACTION}" = "plan" ]; then
+  terraform -chdir=$CONFIG_PATH plan && terraform -chdir=$CONFIG_PATH plan
+fi
+
+if [ "${ACTION}" = "upgrade" ]; then
+  terraform -chdir=$CONFIG_PATH init -upgrade && terraform -chdir=$CONFIG_PATH init -upgrade
+fi
