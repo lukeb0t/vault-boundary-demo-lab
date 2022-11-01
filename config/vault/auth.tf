d61f31584e6fbbba406c6c82aeb3d9dea43ba37a
Luke McCleary <luke.mccleary@gmail.com>
2022-11-01 13:21:01 -0400

    added policies and user for ssh signing demo

--- config/vault/auth.tf
+++ config/vault/auth.tf
@@ -14,3 +14,16 @@ resource "vault_generic_endpoint" "admin" {
 }
 EOT
 }
+
+resource "vault_generic_endpoint" "sec-ops" {
+  depends_on           = [vault_auth_backend.userpass]
+  path                 = "auth/userpass/users/sec-ops"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["default", "ssh-sec-ops"],
+  "password": "rootrootroot"
+}
+EOT
+}
--- config/vault/ssh.tf
+++ config/vault/ssh.tf
@@ -1,11 +1,11 @@
-resource "vault_mount" "ssh-signer" {
+resource "vault_mount" "ssh_client_signer" {
   type        = "ssh"
-  path        = "ssh-signer"
+  path        = "ssh_client_signer"
   description = "SSH engine for signging SSH keys"
 }
 
-resource "vault_ssh_secret_backend_ca" "ssh-signer" {
-    backend = vault_mount.ssh-signer.path
+resource "vault_ssh_secret_backend_ca" "ssh_client-signer" {
+    backend = vault_mount.ssh_client_signer.path
     generate_signing_key = false
     public_key = file("../docker-infra/files/vault_keys.pub")
     private_key = file("../docker-infra/files/vault_keys")
@@ -13,7 +13,7 @@ resource "vault_ssh_secret_backend_ca" "ssh-signer" {
 
 resource "vault_ssh_secret_backend_role" "admin" {
     name          = "admin"
-    backend       = vault_mount.ssh-signer.path
+    backend       = vault_mount.ssh_client_signer.path
     key_type      = "ca"
     default_user  = "admin"
     allowed_users = "*"
@@ -24,16 +24,67 @@ resource "vault_ssh_secret_backend_role" "admin" {
     cidr_list     = "0.0.0.0/0"
 }
 
-resource "vault_policy" "ssh" {
-  name   = "ssh-policy"
-  policy = <<EOT
-path "ssh/*" {
-  capabilities = [ "create", "read", "update", "delete", "list" ]
+resource "vault_ssh_secret_backend_role" "sec_ops" {
+    name          = "security_ops"
+    backend       = vault_mount.ssh_client_signer.path
+    key_type      = "ca"
+    default_user  = "sec_ops"
+    allowed_users = "sec_ops"
+    allowed_extensions = "permit-pty,permit-port-forwarding"
+    default_extensions = {"permit-pty": "", permit-port-forwarding: ""}
+    allow_user_certificates = true
+    ttl = "60m"
+    cidr_list     = "0.0.0.0/0"
 }
 
-# to use the configured SSH secrets engine otp_key_role otp_key_role
-path "ssh/creds/otp_role" {
-  capabilities = ["create", "read", "update"]
+resource "vault_ssh_secret_backend_role" "backup_ops" {
+    name          = "backup_ops"
+    backend       = vault_mount.ssh_client_signer.path
+    key_type      = "ca"
+    default_user  = "backup_ops"
+    allowed_users = "backup_ops"
+    allowed_extensions = "permit-pty,permit-port-forwarding"
+    default_extensions = {"permit-pty": "", permit-port-forwarding: ""}
+    allow_user_certificates = true
+    ttl = "60m"
+    cidr_list     = "0.0.0.0/0"
+}
+
+resource "vault_policy" "ssh_admin" {
+  name   = "ssh-admin"
+  policy = <<EOT
+path "ssh_client_signer/roles" {
+  capabilities = ["list"]
+}
+
+path "ssh_client_signer/sign/admin" {
+  capabilities = ["update", "create"]
+}
+EOT
+}
+
+resource "vault_policy" "ssh_backup_ops" {
+  name   = "ssh-backup-ops"
+  policy = <<EOT
+path "ssh_client_signer/roles" {
+  capabilities = ["list"]
+}
+
+path "ssh_client_signer/sign/backup_ops" {
+  capabilities = ["update", "create"]
+}
+EOT
+}
+
+resource "vault_policy" "ssh_security_ops" {
+  name   = "ssh-sec-ops"
+  policy = <<EOT
+path "ssh_client_signer/roles" {
+  capabilities = ["list"]
+}
+
+path "ssh_client_signer/sign/security_ops" {
+  capabilities = ["update", "create"]
 }
 EOT
 }
